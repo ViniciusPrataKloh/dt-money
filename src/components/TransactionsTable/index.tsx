009a62bfa1d452ae4df2fd4b809be928f5b7846f
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2023-03-24 11:46:03 -0300

    TransactionsTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,31 @@
+import { PriceHighlight, Table, TransactionContainer } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <TransactionContainer>
+            <Table>
+                <tbody>
+
+                    <tr>
+                        <td width="50%">Desenvolvimento de Software</td>
+                        <td>
+                            <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+                        </td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
+
+                    <tr>
+                        <td width="50%">Pizza</td>
+                        <td>
+                            <PriceHighlight variant="outcome">- R$ 60,00</PriceHighlight>
+                        </td>
+                        <td>Alimentação</td>
+                        <td>14/04/2022</td>
+                    </tr>
+
+                </tbody>
+            </Table>
+        </TransactionContainer>
+    )
+}
